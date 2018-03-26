1b9c185d9b8c103c1b71167d1a08557433d677c6
Wagner Rodeski <30899090+wagner-rodeski@users.noreply.github.com>
2018-03-25 21:42:23 -0300

    Add files via upload

---
+++ Itau/temp.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 25 19:55:43 2018
+
+@author: Wagner
+"""
+
+df1 = pd.read_csv('agrup_centroides_Q1.csv')
+df1.head()
+#   k-means
+
+df2 = pd.read_csv('agrupamento_Q1.csv')
+df2.head()
+#   k-means
+
+df3 = pd.read_csv('classificacao_Q3.csv')
+df3.head()
+#   decision tree
+
+df4 = pd.read_csv('classificacao_Q4.csv')
+df4.head()
+#   pca
+#   logistic regression
+
+df6 = pd.read_csv('regressao_Q6.csv')
+df6.head()
+#   random forrest
+
+df7 = pd.read_csv('regressao_Q7.csv')
+df7.head()
+#   random forrest
